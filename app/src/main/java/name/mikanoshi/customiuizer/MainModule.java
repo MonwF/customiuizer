7e7a6874f4598dd3bfdc402d318f3e3760280978
Mikanoshi <iam@mikanoshi.name>
2019-12-18 21:04:11 +0500

    [Fixed][Fingerprint call control] Error during mod activation that was also causing other mods to break

--- app/src/main/java/name/mikanoshi/customiuizer/MainModule.java
+++ app/src/main/java/name/mikanoshi/customiuizer/MainModule.java
@@ -120,9 +120,9 @@ public class MainModule implements IXposedHookZygoteInit, IXposedHookLoadPackage
 			if (mPrefs.getInt("controls_fingerprint1_action", 1) > 1 ||
 				mPrefs.getInt("controls_fingerprint2_action", 1) > 1 ||
 				mPrefs.getInt("controls_fingerprintlong_action", 1) > 1 ||
-				mPrefs.getInt("controls_fingerprint_accept", 1) > 1 ||
-				mPrefs.getInt("controls_fingerprint_reject", 1) > 1 ||
-				mPrefs.getInt("controls_fingerprint_hangup", 1) > 1) Controls.FingerprintEventsHook(lpparam);
+				mPrefs.getStringAsInt("controls_fingerprint_accept", 1) > 1 ||
+				mPrefs.getStringAsInt("controls_fingerprint_reject", 1) > 1 ||
+				mPrefs.getStringAsInt("controls_fingerprint_hangup", 1) > 1) Controls.FingerprintEventsHook(lpparam);
 			if (mPrefs.getInt("controls_backlong_action", 1) > 1 ||
 				mPrefs.getInt("controls_homelong_action", 1) > 1 ||
 				mPrefs.getInt("controls_menulong_action", 1) > 1) Controls.NavBarActionsHook(lpparam);
