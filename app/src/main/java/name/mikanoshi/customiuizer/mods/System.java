3e18d7331b53068e009e3e5217a6f751a549f2b0
MonwF <boluo2@gmail.com>
2022-12-26 23:15:35 +0800

    style: notification slider in miui volume dialog

--- app/src/main/java/name/mikanoshi/customiuizer/mods/System.java
+++ app/src/main/java/name/mikanoshi/customiuizer/mods/System.java
@@ -8738,7 +8738,6 @@ public class System {
                 }
                 else {
                     if (leftLayout.getChildAt(1) != secondLeft) {
-                        Helpers.log("horiz two cols");
                         leftLayout.removeViewAt(1);
                         rightLayout.removeViewAt(0);
                         leftLayout.addView(secondLeft);
