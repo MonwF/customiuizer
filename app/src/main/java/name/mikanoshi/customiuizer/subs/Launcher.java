394908e5dd2d20e306f278d33a558e1df78809c0
MonwF <boluo2@gmail.com>
2023-01-05 22:40:48 +0800

    refactor

--- app/src/main/java/name/mikanoshi/customiuizer/subs/Launcher.java
+++ app/src/main/java/name/mikanoshi/customiuizer/subs/Launcher.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.Preference;
 import android.widget.SeekBar;
 
+import miui.os.Build;
 import name.mikanoshi.customiuizer.R;
 import name.mikanoshi.customiuizer.SubFragment;
 import name.mikanoshi.customiuizer.prefs.CheckBoxPreferenceEx;
@@ -111,15 +112,13 @@ public class Launcher extends SubFragment {
 				findPreference("pref_key_launcher_bottommargin").setEnabled(opt == 1);
 				findPreference("pref_key_launcher_nounlockanim").setEnabled(opt == 1);
 				findPreference("pref_key_launcher_oldlaunchanim").setEnabled(opt == 1);
-				findPreference("pref_key_launcher_googlediscover").setEnabled(opt == 1);
-				findPreference("pref_key_launcher_googleminus").setEnabled(opt == 1);
-				findPreference("pref_key_launcher_closedrawer").setEnabled(opt == 1);
-				Preference pref = findPreference("pref_key_launcher_googleminus");
-				pref.setEnabled(opt == 1);
-				if (!miui.os.Build.IS_INTERNATIONAL_BUILD) {
-					((CheckBoxPreferenceEx)pref).setUnsupported(true);
-					pref.setSummary(R.string.launcher_googleminus_note);
+				CheckBoxPreferenceEx minusPref = findPreference("pref_key_launcher_googleminus");
+				CheckBoxPreferenceEx discoverPref = findPreference("pref_key_launcher_googlediscover");
+				if (Build.IS_INTERNATIONAL_BUILD) {
+					discoverPref.setEnabled(opt == 1);
+					minusPref.setEnabled(opt == 1);
 				}
+				findPreference("pref_key_launcher_closedrawer").setEnabled(opt == 1);
 				break;
 		}
 	}
