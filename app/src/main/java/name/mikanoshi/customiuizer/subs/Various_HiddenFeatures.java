ab96828d22a7e4f7a9f55544d887faf64c5026c7
MonwF <boluo2@gmail.com>
2023-02-18 12:00:03 +0800

    feat: add battery optimization shortcut

--- app/src/main/java/name/mikanoshi/customiuizer/subs/Various_HiddenFeatures.java
+++ app/src/main/java/name/mikanoshi/customiuizer/subs/Various_HiddenFeatures.java
@@ -20,12 +20,21 @@ public class Various_HiddenFeatures extends SubFragment {
 
 		final AppCompatActivity act = (AppCompatActivity) getActivity();
 
-		PreferenceEx aosp = findPreference("pref_key_various_memorystats");
+		PreferenceEx aosp = findPreference("pref_key_various_batteryoptimization");
 		aosp.setCustomSummary("AOSP");
 		aosp.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
 			@Override
 			public boolean onPreferenceClick(Preference preference) {
-				Helpers.launchActivity(act, "com.android.settings", "com.android.settings.Settings$MemorySettingsActivity");
+				Intent intent = new Intent("android.intent.action.MAIN");
+				intent.addCategory(Intent.CATEGORY_DEFAULT);
+				intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+				intent.setComponent(new ComponentName("com.android.settings", "com.android.settings.SubSettings"));
+				intent.putExtra(":settings:show_fragment", "com.android.settings.applications.manageapplications.ManageApplications");
+				Bundle bundle = new Bundle();
+				bundle.putString("classname", "com.android.settings.Settings$HighPowerApplicationsActivity");
+				intent.putExtra(":settings:show_fragment_args", bundle);
+				act.startActivity(intent);
+				act.overridePendingTransition(R.anim.activity_open_enter, R.anim.activity_open_exit);
 				return true;
 			}
 		});
@@ -46,15 +55,35 @@ public class Various_HiddenFeatures extends SubFragment {
 			}
 		});
 
+
+		aosp = findPreference("pref_key_various_memorystats");
+		if (Helpers.isTPlus()) {
+			aosp.setUnsupported(true);
+		}
+		else {
+			aosp.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+				@Override
+				public boolean onPreferenceClick(Preference preference) {
+					Helpers.launchActivity(act, "com.android.settings", "com.android.settings.Settings$MemorySettingsActivity");
+					return true;
+				}
+			});
+		}
+
 		aosp = (PreferenceEx)findPreference("pref_key_various_appusagestats");
 		aosp.setCustomSummary("AOSP");
-		aosp.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-			@Override
-			public boolean onPreferenceClick(Preference preference) {
-				Helpers.launchActivity(act, "com.android.settings", "com.android.settings.UsageStatsActivity");
-				return true;
-			}
-		});
+		if (Helpers.isTPlus()) {
+			aosp.setUnsupported(true);
+		}
+		else {
+			aosp.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+				@Override
+				public boolean onPreferenceClick(Preference preference) {
+					Helpers.launchActivity(act, "com.android.settings", "com.android.settings.UsageStatsActivity");
+					return true;
+				}
+			});
+		}
 
 		aosp = (PreferenceEx)findPreference("pref_key_various_aospnotif");
 		aosp.setCustomSummary("AOSP");
@@ -68,15 +97,18 @@ public class Various_HiddenFeatures extends SubFragment {
 		});
 
 		aosp = (PreferenceEx)findPreference("pref_key_various_aospnotiflog");
-		aosp.setCustomSummary("AOSP");
-		aosp.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-			@Override
-			public boolean onPreferenceClick(Preference preference) {
-				Helpers.launchActivity(act, "com.android.settings", "com.android.settings.Settings$NotificationStationActivity");
-				return true;
-			}
-		});
-
+		if (Helpers.isTPlus()) {
+			aosp.setUnsupported(true);
+		}
+		else {
+			aosp.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+				@Override
+				public boolean onPreferenceClick(Preference preference) {
+					Helpers.launchActivity(act, "com.android.settings", "com.android.settings.Settings$NotificationStationActivity");
+					return true;
+				}
+			});
+		}
 	}
 
 }
